46bf3d6ef2ffcb8c2872e067b25cee872d97b5b9
shrekwang <shrek.wang@gmail.com>
2016-01-12 14:26:52 +0800

    update project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clshow "1.0"
   :description "A Clojure library for showing class members"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/shrekwang/clshow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
